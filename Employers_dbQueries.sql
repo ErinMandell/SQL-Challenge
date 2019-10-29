4968681c52ac671b10300f11867c4be057db92ed
airman89 <52982547+ErinMandell@users.noreply.github.com>
2019-10-29 18:55:45 -0500

    Completed Query file

--- Employers_dbQueries.sql
+++ Employers_dbQueries.sql
@@ -22,3 +22,45 @@ FROM dept_manager  m
 INNER JOIN employees e ON m.emp_no = e.emp_no
 INNER JOIN departments d ON m.dept_no=d.dept_no;
 
+
+-- 4) List the department of each employee with the following information: employee number, last name, 
+-- first name, and department name
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees e
+INNER JOIN dept_emp de ON e.emp_no = de.emp_no
+INNER JOIN departments d ON de.dept_no = d.dept_no;
+
+
+-- 5) List all employees whose first name is "Hercules" and last name begins with "B".
+SELECT *
+FROM employees
+WHERE 
+	first_name = 'Hercules'
+	AND last_name LIKE 'B%';
+
+
+-- 6) List all employees in the Sales department, including their employee number, last name, first name,
+-- and department name.
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees e
+INNER JOIN dept_emp de ON e.emp_no = de.emp_no
+INNER JOIN departments d ON de.dept_no = d.dept_no
+WHERE d.dept_name = 'Sales';
+
+
+-- 7) List all employees in the Sales and Development departments, including their employee number,
+-- last name, first name, and department name.
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees e
+INNER JOIN dept_emp de ON e.emp_no = de.emp_no
+INNER JOIN departments d ON de.dept_no = d.dept_no
+WHERE d.dept_name = 'Sales'
+	OR d.dept_name = 'Development';
+	
+
+-- 8) In descending order, list the frequency count of employee last names, i.e., how many employees
+-- share each last name.
+SELECT last_name, count(last_name) AS "Total per Name"
+FROM employees
+GROUP BY last_name
+ORDER BY last_name ASC;
